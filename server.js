e437f343da0d94da02f70a4408dd7f5c7b49e7ba
unknown <sahuavinash412@gmail.com>
2022-12-12 01:32:46 +0530

    server Created

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log(`Running`);
+const express = require("express"),
+    config = require("config"),
+    bodyParser = require("body-parser"),
+    app = express();
+
+    app.get("/", (req, res) => {
+
+        const data = "Running";
+
+        res.status(200).json({"Message" : "Success", "Data" : data})
+
+        console.log(`Running`);
+    });
+
+    app.listen(config.get("App.port"), () => {
+        console.log(`App Is Running At http://${config.get("App.host")}:${config.get("App.port")}`);
+    });
